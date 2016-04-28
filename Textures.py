c8dcb3ef2a3ba9138ffa96cddf3eeeda90eeadaa
Scrunshes <edouard@menayde.fr>
2016-04-28 20:32:05 +0200

    fix(): Fixed display and now use only points

--- Textures.py
+++ Textures.py
@@ -19,8 +19,7 @@ sources = {
     'angle_left': 'resources/blocks/angle_left.png',
     'double_wall_horizontal': 'resources/blocks/double_wall_horizontal.png',
     'double_wall_vertical': 'resources/blocks/double_wall_vertical.png',
-    'start': 'resources/start.png',
-    'stop': 'resources/stop.png'
+    'point': 'resources/point.png'
 }
 
 for source_name in sorted(sources):
@@ -28,8 +27,7 @@ for source_name in sorted(sources):
         raise ValueError("File %s does not exist." % sources[source_name])
 
 textures = dict([
-    ('start', CoreImage(source=sources['start']).texture),
-    ('stop', CoreImage(source=sources['stop']).texture),
+    ('point', CoreImage(source=sources['point']).texture),
     ('W', CoreImage(source=sources['wall']).texture),
     ('A', CoreImage(source=sources['air']).texture),
     ('L', CoreImage(source=sources['wall_left']).texture),
